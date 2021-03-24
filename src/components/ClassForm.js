30eb44e91c3bc7544538abbadd77685ef861cf9f
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-03-24 14:49:52 -0500

    committing cooking sessions

--- src/components/ClassForm.js
+++ src/components/ClassForm.js
@@ -8,37 +8,61 @@ class ClassForm extends React.Component {
         title: '',
         date: '',
         meeting_link: '',
-        recipe_id: '',
-        host_id: this.props.auth.id,
-        public: ''
+        recipe_id: ''
+        // host_id: this.props.auth.id
     }
 
     handleChange = (e) => {
+        console.log( 'handle change data', e.target.value)
         this.setState({
             [e.target.name]: e.target.value
         })
     }
 
+    handleChangeRecipe = (e) => {
+        console.log( 'recipe data', e.target.value)
+        this.setState({
+            recipe_id: e.target.value
+        })
+    }
+
+
     handleSubmit = (e) => {
         e.preventDefault()
 
-        const newCookingSession = {...this.state}
+        const id = this.props.auth.id
+        //console.log('id', this.props.auth.id)
+
+        const newCookingSession = {...this.state, host_id: id}
 
         const reqObj = {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json'
             },
-            body:  JSON.stringify(newCookingSession)
+            body:  JSON.stringify({cooking_session: newCookingSession})
         }
 
-        fetch('http://localhost:3000/recipes', reqObj)
+        fetch('http://localhost:3000/cooking_sessions', reqObj)
         .then(resp => resp.json())
         .then(newCookingSession => {
+            //console.log('data', newCookingSession)
+            //debugger
           this.props.createCookingSession(newCookingSession)
-          //this.props.history.push('/myclasses')
+          console.log('created session', newCookingSession)
+          this.props.history.push('/myclasses')
         })
     }
+
+
+    handleChangeRecipe = (e) => {
+        console.log( 'recipe data', e.target.value)
+        this.setState({
+            recipe_id: e.target.value
+        })
+    }
+
+
 
     render(){
         return(
@@ -55,25 +79,20 @@ class ClassForm extends React.Component {
                         <input name='date' type= 'datetime-local' value={this.state.date} onChange={this.handleChange} />
                     </Form.Field>
 
-                    {/* <Form.Field>
-                        <label>Time: </label>
-                        <input name='date' type= 'date' format="HH:mm:ss" value={this.state.date} onChange={this.handleChange} />
-                    </Form.Field> */}
+                    <Form.Field>
+                        <label>Recipe: </label>
+                        <select name='recipe_id' value={this.state.recipe_id} onChange={this.handleChangeRecipe}>
+                            {this.props.recipes.map((recipe) => {
+                             return <option value={recipe.id}>{recipe.name}</option>;
+                             })}
+                        </select>
+                    </Form.Field>
 
                     <Form.Field>
                         <label>Meeting Link: </label>
                         <input name='meeting_link' type= 'url' value={this.state.meeting_link} onChange={this.handleChange} placeholder='meeting link' />
                     </Form.Field>
 
-                    <Form.Field>
-                        <label>Select a recipe: </label>
-                        <input name='recipe_id' type= 'text' value={this.state.recipe_id} onChange={this.handleChange} placeholder='recipe link' />
-                    </Form.Field>
-
-                    <Form.Field>
-                        <label>Public Session: </label>
-                        <input name='public' type= 'checkbox' value={this.state.public} onChange={this.handleChange} placeholder='image link' />
-                    </Form.Field>
                     <Button type='submit'>Submit</Button>
                 </form>
 
