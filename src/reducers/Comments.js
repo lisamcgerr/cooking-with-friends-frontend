6ba6819d25119abf3c68976a2805948354fa375c
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-05-12 08:51:36 -0500

    comment added in reducer

--- src/reducers/Comments.js
+++ src/reducers/Comments.js
@@ -1,10 +1,12 @@
-import { LOAD_COMMENTS } from '../actions/actionTypes'
+import { LOAD_COMMENTS, CREATE_COMMENT } from '../actions/actionTypes'
 
 function commentsReducer(state = [], action) 
 {
     switch(action.type) {
         case LOAD_COMMENTS:
             return action.comments
+        case CREATE_COMMENT:
+            return [...state, action.comment]
         default:
             return state
 
