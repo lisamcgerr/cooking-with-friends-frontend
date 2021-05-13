54e2ef9db210c022af3268fd84e205b8572822c8
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-05-13 07:03:18 -0500

    create comment card form

--- src/components/CreateComment.js
+++ src/components/CreateComment.js
@@ -1,29 +1,74 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { createComment } from '../actions/index'
+import { Grid, Form, Button } from 'semantic-ui-react'
 
 class CreateComment extends React.Component {
 
+
+    //grab the id of the recipe off the button -- check class names
     state = {
         post: '',
         user_id: '',
         recipe_id: ''
     }
 
-    // handleInput = (e) => {
-    //     this.setState({
-    //         [e.target.post]: e.target.value,
-    //         user_id: this.props.auth.id,
-    //         recipe_id: 
-    //     })
-    // }
+    handleInput = (e) => {
+        this.setState({
+            [e.target.post]: e.target.value,
+            user_id: this.props.auth.id,
+            recipe_id: this.props.recipe.id
+        })
+    }
+
+    //fetch request tolocalhost3001/comments
+    handleSubmit = (e) => {
+        e.preventDefault()
+
+        const newComment = {...this.state}
+        console.log(newComment)
+
+        const reqObj = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json'
+            },
+            body: JSON.stringify({comment: newComment})
+        }
+        fetch('http://localhost:3000/comments', reqObj)
+        .then(resp => resp.json())
+        .then(data => {
+            this.props.createComment(data)
+            //push comment to page
+            this.props.history.push('/recipes')
+        })
+    }
 
     render(){
         return(
-            <div>
-
-            </div>
+            <> 
+                <Grid  textAlign='center' style={{ height: '100vh' }} verticalAlign='top'>
+                    <Form onSubmit={this.handleSubmit} inverted sixe='large'>
+                        <Form.Input fluid 
+                        icon = ''
+                        iconPosition='left'
+                        name='post'
+                        type='text'
+                        value={this.state.post}
+                        onChange={this.handleInput}
+                        placeholder='leave comment here'
+                        />
+                    </Form>
+                </Grid>
+            </>
         )
+    }
+}
+
+function mapStateToProps(state){
+    return{
+        auth: state.auth
     }
 }
 
@@ -31,4 +76,4 @@ const mapDispatchToProps = {
     createComment: createComment
 }
 
-export default connect(null, mapDispatchToProps)(CreateComment)
+export default connect(mapStateToProps, mapDispatchToProps)(CreateComment)
